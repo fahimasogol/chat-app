9aee8ae1277578faf8832f36682b413218ded64d
fahimasogol <fahimasogol@gmail.com>
2023-12-16 18:50:59 +0330

    user views

--- users/serializers.py
+++ users/serializers.py
@@ -11,6 +11,16 @@ class RegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
 
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with that username already exists.")
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import RegistrationSerializer, LoginSerializer
+
+
+# In RegistrationAPIView, serializer.is_valid() is implicitly called by the CreateAPIView when handling the POST
+# request. The perform_create method is for adding additional operations after the object has been validated and
+# created. In LoginAPIView, since it’s a custom operation and not a standard model operation like
+# create/update/delete, serializer.is_valid() is called explicitly to ensure the data is valid before proceeding with
+# the login process.
+class RegistrationAPIView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        }, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk
+        }, status=status.HTTP_200_OK)
+
+
+class LogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        request.user.auth_token.delete()
+        return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
